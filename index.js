77ae5d2e995e8ae5bd9d390de94bf20f98b52269
Jason Norwood-Young <jason@10layer.com>
2018-02-19 16:31:08 +0200

    Version without puppeteer

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const puppeteer = require('puppeteer');
+const request = require("request-promise-native");
+const cheerio = require('cheerio')
 const restify = require("restify");
 const config = require("config");
 const md5 = require("md5");
@@ -10,183 +11,146 @@ var pages = {};
 
 // Authorization
 var authorization = (req, res, next) => {
-	if (!config.users)
-		return next();
+	if (!config.users) return next();
 	if (!req.authorization.basic)
 		return res.send(403, { status: "error", message: "Forbidden" });
-	var user = config.users.find(user => user.username === req.authorization.basic.username && user.password === req.authorization.basic.password);
-	if (user)
-		return next();
-	res.send(403, { status: "error", message: "Forbidden" })
-}
+	var user = config.users.find(
+		user =>
+			user.username === req.authorization.basic.username &&
+			user.password === req.authorization.basic.password
+	);
+	if (user) return next();
+	res.send(403, { status: "error", message: "Forbidden" });
+};
 
 server.use(authorization);
 
-var _browser = null;
-
-var browserCheck = (req, res, next) => {
-	if (_browser) {
-		req.browser = _browser;
-		return next();
-	}
-	console.log("Launching Puppeteer")
-	console.time("launch-puppeteer")
-	puppeteer.launch(config.puppeteer)
+var loadPage = (page) => {
+	return request(page.url)
 	.then(result => {
-		_browser = req.browser = result;
-		console.timeEnd("launch-puppeteer")
-		next();
-	}).catch(err => {
-		console.error(err)
-		res.send(500, { status: "error", message: err.message })
-	});
-};
-
-server.use(browserCheck);
-
-var pageInfo = page => {
-	// console.log(page)
-	return page.title()
-	.then(title => {
-		return {
-			viewport: page.viewport(),
-			url: page.url(),
-			title
-		}	
-	})
-}
-
-var injectJquery = (page) => {
-	return page.addScriptTag({path: './scripts/jquery-3.2.1.min.js'})
-	.then(result => {
-
-	})
+		page.date_updated = new Date();
+		page.content = result;
+		return page;
+	});		
 }
 
 server.post("/open", (req, res) => {
 	if (!req.body.url) {
-		console.error("url required")
-		return res.send(500, { status: "error", message: "url required" })
+		console.error("url required");
+		return res.send(500, { status: "error", message: "url required" });
 	}
-	console.time("opened " + req.body.url);
-	const _id = md5(req.username + req.body.url);
-	var page = pages[_id];
-	if (page)
-		return res.send({ status: "ok", message: "page already open", url: page.url, _id, date_created: page.date_created, date_updated: page.date_updated })
-	req.browser.newPage()
-	.then(result => {
-		page = result;
-		return page.setRequestInterception(true);
-	})
-	.then(result => {
-		page.on('request', request => {
-			if (!config.loadTypes)
-				return request.continue();
-			if (config.loadTypes.indexOf(request.resourceType) !== -1)
-				request.continue();
-			else
-				request.abort();
+	var url = req.body.url;
+	console.time("opened " + url);
+	const _id = md5(req.username + url);
+	if (pages[_id])
+		return res.send({
+			status: "ok",
+			message: "page already open",
+			url,
+			_id,
+			date_created: pages[_id].date_created,
+			date_updated: pages[_id].date_updated
 		});
-		return page.goto(req.body.url);
-	})
+	var page = {
+		_id,
+		username: req.username,
+		url,
+		date_created: new Date(),
+		date_updated: new Date(),
+		reload_secs: req.body.reload
+	}
+	loadPage(page)
 	.then(result => {
-		url = result.url;
-		return injectJquery(page);
-	})
-	.then(result => {
-		pages[_id] = { username: req.username, url, page, date_created: new Date(), date_updated: new Date() };
-		if (req.body.reload) {
-			pages[_id].reloadTimer = setInterval(() => {
-				console.log("Reloading", pages[_id].url);
-				pages[_id].page.reload()
-				.then(result => {
-					pages[_id].date_updated = new Date();
-					return injectJquery(pages[_id].page);
-				});
-			}, (req.body.reload * 1000))
-		}
-		console.timeEnd("opened " + req.body.url);
-		res.send({ status: "ok", message: "page open", url, _id, date_created: page.date_created, date_updated: page.date_updated })
+		pages[_id] = result;
+		console.timeEnd("opened " + url);
+		res.send({
+			status: "ok",
+			message: "page open",
+			url,
+			_id
+		});
 	})
 	.catch(err => {
-		console.error(err)
-		res.send(500, { status: "error", message: err.message })
+		console.error(err);
+		res.send(500, {
+			status: "error",
+			message: err
+		});
 	})
-})
+});
 
 server.get("/page", (req, res) => {
-	req.browser.pages()
-	.then(pages => {
-		res.send({ status: "ok", pages: pages.map(pageInfo) })
-	})
-	.catch(err => {
-		console.error(err)
-		res.send(500, { status: "error", message: err.message })
-	})
-})
+	res.send({ status: "ok", pages });	
+});
 
 var getPage = (req, res, next) => {
 	if (!pages[req.params.page_id]) {
-		console.error("Page not found")
-		res.send(500, { status: "error", message: "Page not found - please send _id in url" })
+		console.error("Page not found");
+		res.send(500, {
+			status: "error",
+			message: "Page not found - please send _id in url"
+		});
 		return;
 	}
-	req.page = pages[req.params.page_id].page;
+	req.page = pages[req.params.page_id];
 	next();
-}
+};
 
 server.get("/page/:page_id", getPage, (req, res) => {
 	res.send(req.page);
-})
+});
 
-server.post("/page/:page_id/text", getPage, (req, res) => {
-	if (!req.body.selector) {
-		return res.send(500, { status: "error", message: "selector required" })
+var ensureSelector = (req, res, next) => {
+	if (!req.body || !req.body.selector) {
+		return res.send(500, { status: "error", message: "selector required" });
 	}
+	next();
+}
+server.post("/page/:page_id/text", ensureSelector, getPage, (req, res) => {
 	let selector = req.body.selector;
-	req.page.evaluate((selector) => {
-		try {
-			const $ = window.$;
-			return $(selector).text();
-		} catch(e) {
-			console.error(e);
-			return Promise.resolve(null);
-		}
-	}, selector)
-	.then(text => {
-		res.send({ status: "ok", text });
-	})
-	.catch(err => {
-		console.error(err)
-		res.send(500, { status: "error", message: err.message })
-	})
-})
+	$ = cheerio.load(req.page.content);
+	try {
+		console.log({ url: req.page.url, selector });
+		var text = $(selector).text();
+	} catch(err) {
+		console.error(err);
+		return res.send(500, { status: "error", message: "error finding text for url " + req.page.url});
+	}
+	res.send({ status: "ok", text });
+});
 
-server.post("/page/:page_id/attr", getPage, (req, res) => {
-	if (!req.body.selector) {
-		return res.send(500, { status: "error", message: "selector required" })
-	}
-	if (!req.body.attr) {
-		return res.send(500, { status: "error", message: "attr required" })
-	}
+server.post("/page/:page_id/attr", ensureSelector, getPage, (req, res) => {
 	let selector = req.body.selector;
-	let attr = req.body.attr;
-	req.page.evaluate((selector, attr) => {
-		const $ = window.$;
-		return $(selector).attr(attr);
-	}, selector, attr)
-	.then(text => {
-		res.send({ status: "ok", text });
-	})
-	.catch(err => {
-		console.error(err)
-		res.send(500, { status: "error", message: err.message })
-	})
-})
+	$ = cheerio.load(req.page.content);
+	try {
+		var attr = $(selector).attr();
+	} catch(err) {
+		console.error(err);
+		return res.send(500, { status: "error", message: "error finding attr for url " + req.page.url});
+	}
+	return res.send({ status: "ok", attr });
+});
 
+server.post("/page/:page_id/html", ensureSelector, getPage, (req, res) => {
+	let selector = req.body.selector;
+	$ = cheerio.load(req.page.content);
+	try {
+		var html = $(selector).html();
+	} catch(err) {
+		console.error(err);
+		return res.send(500, { status: "error", message: "error finding html for url " + req.page.url});
+	}
+	return res.send({ status: "ok", html });
+});
 
 server.listen(config.server.port, function() {
-	console.log('%s listening at %s', server.name, server.url);
+	console.log("%s listening at %s", server.name, server.url);
 });
+
+// var timer = setInterval(() => {
+// 	pages.forEach(page => {
+
+// 	})
+// }, 10000)
 
 module.exports = server;
--- test/puppeteer-rest-test.js
+++ test/puppeteer-rest-test.js
@@ -28,62 +28,20 @@ describe('Basics', () => {
 		});
 	});
 
-	// describe("/POST page", () => {
-	// 	it("it should open a new page with a specific url", (done) => {
-	// 		chai.request(server)
-	// 		.post("/page")
-	// 		.send({ url: "https://www.google.com"})
-	// 		.end((err, res) => {
-	// 			res.should.have.status(200);
-	// 			res.body.status.should.equal('ok');
-	// 			res.body.message.should.equal('page open')
-	// 			res.body.should.have.property('url')
-	// 			res.body.should.have.property('pagenumber')
-	// 			done();
-	// 		});
-	// 	});
-	// })
-
-	// describe("/GET page/:pagenumber", () => {
-	// 	it("it should switch to a specific page", (done) => {
-	// 		chai.request(server)
-	// 		.get("/page/0")
-	// 		.end((err, res) => {
-	// 			res.should.have.status(200);
-	// 			res.body.status.should.equal('ok');
-	// 			res.body.message.should.equal('page active')
-	// 			res.body.should.have.property('url')
-	// 			done();
-	// 		});
-	// 	});
-	// })
-
-	// describe("/GET page/count", () => {
-	// 	it("it should count open pages", (done) => {
-	// 		chai.request(server)
-	// 		.get("/page/count")
-	// 		.end((err, res) => {
-	// 			res.should.have.status(200);
-	// 			res.body.status.should.equal('ok');
-	// 			res.body.count.should.equal(1)
-	// 			done();
-	// 		});
-	// 	});
-	// })
-
 	describe("/GET page", () => {
 		it("should get info on all the pages", done => {
 			chai.request(server)
 			.get("/page")
 			.end((err, res) => {
+				// console.log(res.body)
 				res.should.have.status(200);
 				res.body.status.should.equal('ok');
 				res.body.should.have.property('pages')
-				res.body.pages.should.be.an("array")
+				res.body.pages.should.be.an("object")
 				done();
 			});
 		})
-	})
+	});
 
 	describe("/POST text", () => {
 		it("should get text based on selector", done => {
@@ -98,7 +56,7 @@ describe('Basics', () => {
 				done();
 			});
 		})
-	})
+	});
 
 	describe("/POST attr", () => {
 		it("should get attr based on selector", done => {
@@ -106,39 +64,14 @@ describe('Basics', () => {
 			.post(`/page/${ page_id }/attr`)
 			.send({ selector: "#hplogo", attr: "src" })
 			.end((err, res) => {
+				console.log(res.body)
 				res.should.have.status(200);
 				res.body.status.should.equal('ok');
-				res.body.should.have.property('text')
-				res.body.text.should.be.a("string")
+				res.body.should.have.property('attr')
+				res.body.attr.should.be.a("object")
 				done();
 			});
 		})
-	})
-
-	// describe("/DELETE page/:pagenumber", () => {
-	// 	it("it should close a page", (done) => {
-	// 		chai.request(server)
-	// 		.del("/page/0")
-	// 		.end((err, res) => {
-	// 			res.should.have.status(200);
-	// 			res.body.status.should.equal('ok');
-	// 			res.body.message.should.equal('page closed')
-	// 			done();
-	// 		});
-	// 	});
-	// })
-
-	// describe("/GET close", () => {
-	// 	it("it should close puppeteer", (done) => {
-	// 		chai.request(server)
-	// 		.get("/close")
-	// 		.end((err, res) => {
-	// 			res.should.have.status(200);
-	// 			res.body.status.should.equal('ok');
-	// 			res.body.message.should.equal('puppeteer closed')
-	// 			done();
-	// 		});
-	// 	});
-	// });
+	});
 
 });
